dd28c721b672e2c7d58b664a2b551ef28d335f3a
RiggedVR <mythicalityyt1@gmail.com>
2022-08-20 18:13:56 +0100

    Added Havenite Ores

--- src/main/java/com/rigged/riggedcraft/block/ModBlocks.java
+++ src/main/java/com/rigged/riggedcraft/block/ModBlocks.java
@@ -6,17 +6,27 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 
 public class ModBlocks {
 
-    public static final Block HAVENIUM_BLOCK = registerBlock("havenium_block",
+    public static final Block HAVENITE_BLOCK = registerBlock("havenite_block",
             new Block(FabricBlockSettings.of(Material.METAL).slipperiness(.989f).strength(4f).requiresTool()), ModItemsGroup.RIGGEDCRAFT);
+
+    public static final Block HAVENITE_ORE = registerBlock("havenite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3f).requiresTool(),
+                    UniformIntProvider.create(3, 7)), ModItemsGroup.RIGGEDCRAFT);
+
+    public static final Block DEEPSLATE_HAVENITE_ORE = registerBlock("deepslate_havenite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3f).requiresTool(),
+                    UniformIntProvider.create(3, 7)), ModItemsGroup.RIGGEDCRAFT);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
